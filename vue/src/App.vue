4000b0f71c2801ee34e0db84cf4d5692f2a0f893
Dada <detazamarkdaryl2014@gmail.com>
2022-09-01 16:41:38 +0800

    updated navbar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,31 +1,433 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Popover class="relative bg-white">
+    <div class="mx-auto px-4 sm:px-8">
+      <div
+        class="flex items-center justify-between border-b-2 border-gray-100 py-3 md:justify-start md:space-x-10"
+      >
+        <div class="flex justify-start lg:w-0 lg:flex-1">
+          <a href="#" class="z-20">
+            <span class="sr-only">Province</span>
+            <img
+              class="h-24 w-auto sm:h-24"
+              src="./assets/img/province_logo_colored.gif"
+              alt=""
+            />
+          </a>
+          <a href="#" class="mt-2 ml-2 z-10">
+            <span class="sr-only">Congress</span>
+            <img
+              class="h-20 w-auto sm:h-20"
+              src="./assets/img/logo_colored.gif"
+              alt=""
+            />
+          </a>
+          <a href="#" class="mt-4 -ml-28 z-0">
+            <span class="sr-only">Congress</span>
+            <img
+              class="h-20 w-auto sm:h-20"
+              src="./assets/img/title.gif"
+              alt=""
+            />
+          </a>
+        </div>
+        <div class="-my-2 -mr-2 md:hidden">
+          <PopoverButton
+            class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          >
+            <span class="sr-only">Open menu</span>
+            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+          </PopoverButton>
+        </div>
+        <PopoverGroup as="nav" class="hidden space-x-10 md:flex">
+          <Popover class="relative" v-slot="{ open }">
+            <PopoverButton
+              :class="[
+                open ? 'text-gray-900' : 'text-gray-500',
+                'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
+              ]"
+            >
+              <span class="prose">JPM Assistance</span>
+              <ChevronDownIcon
+                :class="[
+                  open ? 'text-gray-600' : 'text-gray-400',
+                  'ml-2 h-5 w-5 group-hover:text-gray-500',
+                ]"
+                aria-hidden="true"
+              />
+            </PopoverButton>
+
+            <transition
+              enter-active-class="transition ease-out duration-200"
+              enter-from-class="opacity-0 translate-y-1"
+              enter-to-class="opacity-100 translate-y-0"
+              leave-active-class="transition ease-in duration-150"
+              leave-from-class="opacity-100 translate-y-0"
+              leave-to-class="opacity-0 translate-y-1"
+            >
+              <PopoverPanel
+                class="absolute -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2"
+              >
+                <div
+                  class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5"
+                >
+                  <div
+                    class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8"
+                  >
+                    <a
+                      v-for="item in solutions"
+                      :key="item.name"
+                      :href="item.href"
+                      class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                    >
+                      <component
+                        :is="item.icon"
+                        class="h-6 w-6 flex-shrink-0 text-indigo-600"
+                        aria-hidden="true"
+                      />
+                      <div class="ml-4">
+                        <p class="text-base font-medium text-gray-900">
+                          {{ item.name }}
+                        </p>
+                        <p class="mt-1 text-sm text-gray-500">
+                          {{ item.description }}
+                        </p>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </PopoverPanel>
+            </transition>
+          </Popover>
+
+          <a
+            href="#"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
+            >News</a
+          >
+          <a
+            href="#"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
+            >Articles</a
+          >
+          <a
+            href="#"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
+            >Gallery</a
+          >
+
+          <Popover class="relative" v-slot="{ open }">
+            <PopoverButton
+              :class="[
+                open ? 'text-gray-900' : 'text-gray-500',
+                'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
+              ]"
+            >
+              <span>More</span>
+              <ChevronDownIcon
+                :class="[
+                  open ? 'text-gray-600' : 'text-gray-400',
+                  'ml-2 h-5 w-5 group-hover:text-gray-500',
+                ]"
+                aria-hidden="true"
+              />
+            </PopoverButton>
+
+            <transition
+              enter-active-class="transition ease-out duration-200"
+              enter-from-class="opacity-0 translate-y-1"
+              enter-to-class="opacity-100 translate-y-0"
+              leave-active-class="transition ease-in duration-150"
+              leave-from-class="opacity-100 translate-y-0"
+              leave-to-class="opacity-0 translate-y-1"
+            >
+              <PopoverPanel
+                class="absolute left-1/2 z-40 mt-3 w-screen max-w-md -translate-x-1/2 transform px-2 sm:px-0"
+              >
+                <div
+                  class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5"
+                >
+                  <div
+                    class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8"
+                  >
+                    <a
+                      v-for="item in resources"
+                      :key="item.name"
+                      :href="item.href"
+                      class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                    >
+                      <component
+                        :is="item.icon"
+                        class="h-6 w-6 flex-shrink-0 text-indigo-600"
+                        aria-hidden="true"
+                      />
+                      <div class="ml-4">
+                        <p class="text-base font-medium text-gray-900">
+                          {{ item.name }}
+                        </p>
+                        <p class="mt-1 text-sm text-gray-500">
+                          {{ item.description }}
+                        </p>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </PopoverPanel>
+            </transition>
+          </Popover>
+        </PopoverGroup>
+        <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+          <a
+            href="#"
+            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+            >Sign in</a
+          >
+          <a
+            href="#"
+            class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+            >Sign up</a
+          >
+        </div>
+      </div>
+    </div>
+
+    <transition
+      enter-active-class="duration-200 ease-out"
+      enter-from-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="duration-100 ease-in"
+      leave-from-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
+    >
+      <PopoverPanel
+        focus
+        class="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden z-50"
+      >
+        <div
+          class="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+        >
+          <div class="px-5 pt-5 pb-6">
+            <div class="flex items-center justify-between">
+              <div>
+                <img
+                  class="h-16 w-auto"
+                  src="./assets/img/logo_colored.gif"
+                  alt="Workflow"
+                />
+              </div>
+              <div class="-mr-2">
+                <PopoverButton
+                  class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                >
+                  <span class="sr-only">Close menu</span>
+                  <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                </PopoverButton>
+              </div>
+            </div>
+            <div class="mt-6">
+              <nav class="grid gap-y-8">
+                <a
+                  v-for="item in solutions"
+                  :key="item.name"
+                  :href="item.href"
+                  class="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
+                >
+                  <component
+                    :is="item.icon"
+                    class="h-6 w-6 flex-shrink-0 text-indigo-600"
+                    aria-hidden="true"
+                  />
+                  <span class="ml-3 text-base font-medium text-gray-900">{{
+                    item.name
+                  }}</span>
+                </a>
+              </nav>
+            </div>
+          </div>
+          <div class="space-y-6 py-6 px-5">
+            <div class="grid grid-cols-2 gap-y-4 gap-x-8">
+              <a
+                href="#"
+                class="text-base font-medium text-gray-900 hover:text-gray-700"
+                >News</a
+              >
+
+              <a
+                href="#"
+                class="text-base font-medium text-gray-900 hover:text-gray-700"
+                >Gallery</a
+              >
+              <a
+                href="#"
+                class="text-base font-medium text-gray-900 hover:text-gray-700"
+                >Articles</a
+              >
+              <a
+                v-for="item in resources"
+                :key="item.name"
+                :href="item.href"
+                class="text-base font-medium text-gray-900 hover:text-gray-700"
+                >{{ item.name }}</a
+              >
+            </div>
+            <div>
+              <a
+                href="#"
+                class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                >Sign up</a
+              >
+              <p class="mt-6 text-center text-base font-medium text-gray-500">
+                Existing user?
+                {{ " " }}
+                <a href="#" class="text-indigo-600 hover:text-indigo-500"
+                  >Sign in</a
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+      </PopoverPanel>
+    </transition>
+  </Popover>
+  <main>
+    <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      <div>
+        <div className="text-center pb-3 pt-4">
+          <h1
+            className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+            data-aos="zoom-y-out"
+          >
+            JCA for Progress
+            <span
+              className="bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-teal-400"
+              >Movement</span
+            >
+          </h1>
+          <div className="max-w-3xl mx-auto">
+            <p
+              className="text-xl text-gray-600 mb-8"
+              data-aos="zoom-y-out"
+              data-aos-delay="150"
+            >
+              An Organization started by Jose Chavez Alvares to help the
+              community to have better way of living.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <!-- <div class="carousel w-full h-96">
+    <div id="slide1" class="carousel-item relative w-full">
+      <img src="./assets/img/1.jpg" class="w-full" />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide4" class="btn btn-circle">❮</a>
+        <a href="#slide2" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+    <div id="slide2" class="carousel-item relative w-full">
+      <img src="./assets/img/2.jpg" class="w-full" />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide1" class="btn btn-circle">❮</a>
+        <a href="#slide3" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+    <div id="slide3" class="carousel-item relative w-full">
+      <img src="./assets/img/3.jpg" class="w-full" />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide2" class="btn btn-circle">❮</a>
+        <a href="#slide4" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+    <div id="slide4" class="carousel-item relative w-full">
+      <img src="./assets/img/4.jpg" class="w-full" />
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide3" class="btn btn-circle">❮</a>
+        <a href="#slide1" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+  </div> -->
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import {
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from "@headlessui/vue";
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  BookmarkSquareIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  CursorArrowRaysIcon,
+  LifebuoyIcon,
+  PhoneIcon,
+  PlayIcon,
+  ShieldCheckIcon,
+  Squares2X2Icon,
+  XMarkIcon,
+} from "@heroicons/vue/24/outline";
+import { ChevronDownIcon } from "@heroicons/vue/20/solid";
+
+const solutions = [
+  {
+    name: "Financial Assitance",
+    description: "Apply for financial assistance that you might need.",
+    href: "#",
+    icon: ChartBarIcon,
+  },
+  {
+    name: "Scholarship",
+    description: "Get an opportunity to be part of our scholarship programs.",
+    href: "#",
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: "Livelihood",
+    description: "Join in our livelihood seminars and training programs.",
+    href: "#",
+    icon: ShieldCheckIcon,
+  },
+];
+
+const resources = [
+  {
+    name: "Guides",
+    description:
+      "Learn how to maximize our platform to get the most out of it.",
+    href: "#",
+    icon: BookmarkSquareIcon,
+  },
+  {
+    name: "Events",
+    description: "See what events we might be planning near you.",
+    href: "#",
+    icon: CalendarIcon,
+  },
+  {
+    name: "Frequently Ask Questions",
+    description:
+      "Get all of your questions answered in our forums or contact support.",
+    href: "#",
+    icon: LifebuoyIcon,
+  },
+  {
+    name: "About Us",
+    description: "Get to know who we are and what we do.",
+    href: "#",
+    icon: ShieldCheckIcon,
+  },
+];
+</script>
