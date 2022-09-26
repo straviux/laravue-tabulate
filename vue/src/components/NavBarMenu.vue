da526c4a70a775da98661a7e537bb40226f85f65
straviux <arvin.heredero@gmail.com>
2022-09-27 00:30:26 +0800

    ..

--- vue/src/components/NavBarMenu.vue
+++ vue/src/components/NavBarMenu.vue
@@ -5,7 +5,7 @@
         :to="to"
         class="p-3 flex justify-start items-center gap-2"
         active-class="font-bold text-gray-600 drop-shadow-md"
-        :class="[this.$route.name == to.name ? '' : 'text-gray-500']"
+        :class="[route.name == to.name ? '' : 'text-gray-500']"
       >
         <mdicon :name="icon" class="text-current" /><span>{{ text }}</span>
       </router-link>
@@ -13,6 +13,9 @@
   </ul>
 </template>
 <script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
 const items = [
   { text: "Dashboard", icon: "view-dashboard", to: { name: "Dashboard" } },
   {
