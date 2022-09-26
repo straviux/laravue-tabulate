c520c40314c7be7d969a1774ceb853f9d67af0ee
straviux <arvin.heredero@gmail.com>
2022-09-27 00:31:05 +0800

    ..

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios"
 import store from "./store"
 
-
-
 const axiosClient = axios.create({
   // baseURL: 'http://192.168.0.170:8000/api'
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+  baseURL: 'http://localhost:8000/api'
 })
 
 
